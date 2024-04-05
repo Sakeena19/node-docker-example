688b215993d8c97f1d9ee90efebd296447c0bd5d
Ubuntu <ubuntu@ip-172-31-43-17.ec2.internal>
2024-04-05 23:49:42 +0000

    Adding nodejs app code

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js
+
+const http = require('http');
+
+const hostname = '34.207.55.189';
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
