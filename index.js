b45cbb3fa0cdbd3aadc598010e711d70c5a7d40f
Sakeena19 <133941373+Sakeena19@users.noreply.github.com>
2024-04-06 15:29:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const http = require('http');
 
-const hostname = '3.86.47.238';
+const hostname = '172.20.128.1';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
